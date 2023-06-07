b9750813ecbf0b28e89bdb0767a8522f5818f915
Koykoy200078 <franc200078@gmail.com>
2023-06-07 23:31:55 +0800

    update

--- App.js
+++ App.js
@@ -1,20 +1,141 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useRef, useEffect } from 'react';
+import {
+  SafeAreaView,
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+  Platform,
+  BackHandler,
+  TouchableOpacity,
+  Dimensions,
+} from 'react-native';
+import { WebView } from 'react-native-webview';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const { width } = Dimensions.get('window');
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasError, setHasError] = useState(false);
+  const [url, setUrl] = useState('http://192.168.1.206:8000');
+
+  const handleLoadStart = () => {
+    setIsLoading(true);
+  };
+
+  const handleLoadEnd = () => {
+    setIsLoading(false);
+  };
+
+  const handleLoadError = () => {
+    setHasError(true);
+    setIsLoading(false);
+  };
+
+  const handleRetry = () => {
+    setHasError(false);
+    setIsLoading(true);
+    if (webViewRef.current) {
+      webViewRef.current.reload();
+    }
+  };
+
+  const webViewRef = useRef(null);
+  const onAndroidBackPress = () => {
+    if (webViewRef.current) {
+      webViewRef.current.goBack();
+      return true;
+    }
+    return false;
+  };
+
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      BackHandler.addEventListener('hardwareBackPress', onAndroidBackPress);
+      return () => {
+        BackHandler.removeEventListener(
+          'hardwareBackPress',
+          onAndroidBackPress
+        );
+      };
+    }
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar
+        style="auto"
+        translucent={true}
+        backgroundColor={'transparent'}
+      />
+
+      {isLoading && (
+        <View style={styles.activityIndicatorContainer}>
+          <ActivityIndicator size="large" color="#0000ff" />
+        </View>
+      )}
+      {hasError && (
+        <View style={styles.errorContainer}>
+          <Text style={styles.errorText}>Something went wrong.</Text>
+          <TouchableOpacity onPress={handleRetry}>
+            <View
+              style={{
+                borderWidth: 1,
+                padding: 10,
+                borderRadius: 10,
+                width: width / 2,
+                height: 50,
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <Text style={{ fontSize: 15 }}>Retry</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )}
+      {!hasError && (
+        <WebView
+          source={{ uri: url }}
+          onLoadStart={handleLoadStart}
+          onLoadEnd={handleLoadEnd}
+          onError={handleLoadError}
+          ref={webViewRef}
+          sharedCookiesEnabled={true}
+          onMessage={(event) => {
+            console.log(event.nativeEvent.data);
+          }}
+          style={{ flex: 1, borderWidth: 2, borderColor: 'red' }}
+          scalesPageToFit={true}
+          showsVerticalScrollIndicator={false}
+          showsHorizontalScrollIndicator={false}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    marginTop: Constants.statusBarHeight,
+  },
+  activityIndicatorContainer: {
+    flex: 1,
     justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%',
+  },
+  errorContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  errorText: {
+    fontSize: 20,
+    marginBottom: 20,
   },
 });
